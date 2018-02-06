ab2ab9a5bb047c86a5d417b00446b719b903856c
rottik <rott@4m.to>
2018-02-06 01:15:28 +0100

    Added spectrum and capstrum normalization.

--- barcode_blur.m
+++ barcode_blur.m
@@ -2,6 +2,8 @@
 clear all
 close all
 
+feature('SetPrecision', 24)
+
 % img=imread('tmp583977.png');
 % gray=rgb2gray(img);
 % gray_sharp=gray(44:374,30:568);
@@ -18,43 +20,33 @@ close all
 % subplot(2,2,4)
 % imshow(cepstrum)
 % figure
-padding=100;
 img=imread('barcode.png');
 img=img(10:end,10:end,:);
-gray=zeros(size(img,1)+padding,size(img,2)+padding,'uint8');
-gray(padding/2:end-padding/2-1,padding/2:end-padding/2-1)=gray(padding/2:end-padding/2-1,padding/2:end-padding/2-1)+rgb2gray(img);
+gray=rgb2gray(img);
 
 h2d=hamming(size(gray,1))*hamming(size(gray,2))';
 spect=fft2(double(gray).*h2d,size(gray,1),size(gray,2));
-%spect=round(real(spect)*10000)/10000 + round(imag(spect)*10000)/10000;
-cepstrum=fft2(log(abs(spect).^2));
-spect=spect./(size(spect,1)*size(spect,2));
+spect=round(spect*1000000)/1000000;
+spect=spect./((size(spect,1)*size(spect,2)));
+cepstrum=real(fft2(log(abs(spect).^2)));
+cepstrum=cepstrum./((size(cepstrum,1)*size(cepstrum,2)));
+
 c0v=cepstrum(1,:);
 R=0;
 
-for r=1:length(c0v)
+for r=2:length(c0v)
     if(c0v(r)<0)
         R=r/2;
         break;
     end
 end
+R
 
-h=zeros(R*2,R*2);
-for x=1:size(h,1)
-    for y=1:size(h,2)
-        X=(x-R/2);
-        Y=(y-R/2);
-        if(X*X+Y*Y<=R*R)
-            h(x,y)=0.5*pi*R;
-        end
-    end
-end
 
-H=fft2(h);
 
 subplot(2,2,1)
 imshow(gray)
-subplot(2,2,2)
+%subplot(2,2,2)
 
 subplot(2,2,3)
 imshow(fftshift(abs(spect)))
